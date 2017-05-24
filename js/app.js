7d3a0147a51b77f048914747d160bea267d2c0f3
Arowain <434100131@student.ksu.edu.sa>
2017-05-24 14:31:04 +0300

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -5,17 +5,19 @@ var viewModel = {
     imagePath: ko.observable(""),
     pointMarker: function(data) {
         //http://knockoutjs.com/documentation/click-binding.html#note-1-passing-a-current-item-as-a-parameter-to-your-handler-function
-        if (initMap.largeInfowindow.marker != data.location) {
+      /*  if (initMap.largeInfowindow.marker != data.location) {
         for (var i = 0; i < markers.length; i++) {
             if (markers[i].title == data.title) {
                 populateInfoWindow(markers[i], infoWindow);
                 break;
             }
         }
-    }
-      
+    }*/
+    console.log(data);
+    google.maps.event.trigger(data.marker, 'click');
+
+
     }
 };
-
 //ViewModel.query.subscribe(ViewModel.search);45
 ko.applyBindings(viewModel);
